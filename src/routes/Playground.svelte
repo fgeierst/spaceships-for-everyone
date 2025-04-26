03900893a7f57516226deeb16a5237d8536c8b47
Florian Geierstanger <florian.geierstanger@codecentric.de>
2025-04-26 16:32:41 +0200

    remove playground elements

--- src/routes/Playground.svelte
+++ src/routes/Playground.svelte
@@ -5,25 +5,5 @@
 	onMount(() => {});
 </script>
 
-<svelte:head>
-	<script type="module" src="node_modules/playground-elements/playground-ide.js"></script>
-</svelte:head>
-
-<link rel="stylesheet" href="/node_modules/playground-elements/themes/neo.css" />
-
-<playground-ide
-	id="playground"
-	resizable
-	editable-file-system
-	line-numbers
-	line-wrapping
-	class="playground-theme-neo"
-	project-src={projectSrc}
->
-</playground-ide>
-
 <style>
-	#playground {
-		--playground-code-font-family: Roboto Mono, monospace;
-	}
 </style>
