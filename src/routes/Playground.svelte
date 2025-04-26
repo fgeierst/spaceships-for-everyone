dc0d2b1ded89fd06c1fdad4a2892946cb694582a
Florian Geierstanger <florian.geierstanger@codecentric.de>
2025-04-26 16:42:00 +0200

    codemirror

--- src/routes/Playground.svelte
+++ src/routes/Playground.svelte
@@ -1,9 +1,38 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
+	import { EditorView, basicSetup } from 'codemirror';
+	import { javascript } from '@codemirror/lang-javascript';
 
+	const id = $props.id();
 	const projectSrc = $state('/chapters/button.json');
-	onMount(() => {});
+	let editor: EditorView | null = null;
+
+	onMount(() => {
+		editor = new EditorView({
+			doc: 'Hello world',
+			extensions: [
+				basicSetup,
+				javascript({
+					jsx: true,
+					typescript: true
+				}),
+				EditorView.lineWrapping,
+				EditorView.updateListener.of((update) => {
+					if (update.docChanged) {
+						console.log('Document changed:', update.state.doc.toString());
+					}
+				})
+			],
+			parent: document.querySelector(`#editor-${id}`)!
+		});
+
+		return () => {
+			editor?.destroy();
+		};
+	});
 </script>
+
+<div id={`editor-${id}`}></div>
 
 <style>
 </style>
