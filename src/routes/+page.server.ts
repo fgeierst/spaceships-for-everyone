91d69c15679393d774bf908ac56af774f1970b9d
Florian Geierstanger <florian.geierstanger@codecentric.de>
2025-05-01 13:25:58 +0200

    hardcode chapters

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,14 @@
 import type { PageServerLoad } from './$types';
-import fs from 'fs';
-
-export const prerender = true;
+// import fs from 'fs';
 
 export const load = (async () => {
-	const files = fs.readdirSync('static/chapters');
-	const chapters: string[] = [];
-	files.forEach((f) => {
-		if (f.includes('solution') || f.includes('.mdx')) return null;
-		chapters.push(f.replace('.html', ''));
-	});
+	// can't access file system in vercel https://svelte.dev/docs/kit/adapter-vercel#Troubleshooting-Accessing-the-file-system
+	// const files = fs.readdirSync('static/chapters');
+	// const chapters: string[] = [];
+	// files.forEach((f) => {
+	// 	if (f.includes('solution') || f.includes('.mdx')) return null;
+	// 	chapters.push(f.replace('.html', ''));
+	// });
+	const chapters = ['button', 'hello-world', 'web-component'];
 	return { chapters };
 }) satisfies PageServerLoad;
