ee273255a9ee89ec8a731ec9d83d47c2d81da3e5
Florian Geierstanger <florian.geierstanger@codecentric.de>
2025-05-02 11:16:43 +0200

    Wcag component

---
+++ src/lib/components/Wcag.stories.svelte
@@ -0,0 +1,23 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Wcag from './Wcag.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Wcag',
+		component: Wcag,
+		args: {
+			ref_id: '1.1.1',
+			variant: 'link'
+		}
+	});
+</script>
+
+<Story name="Link" />
+
+<Story
+	name="Card"
+	args={{
+		ref_id: '1.4.3',
+		variant: 'card'
+	}}
+/>
---
+++ src/lib/components/Wcag.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import wcag from 'wcag-as-json/src/wcag.json';
+
+	type Props = {
+		ref_id: string;
+		variant?: 'link' | 'card';
+	};
+	const { ref_id = '1.1.1', variant = 'link' }: Props = $props();
+
+	const [mainNumber, guidelineNumber] = ref_id.split('.');
+	const mainGuideline = wcag.find((item) => item.ref_id === mainNumber);
+	const guideline = mainGuideline?.guidelines?.find(
+		(g) => g.ref_id === `${mainNumber}.${guidelineNumber}`
+	);
+	const criterion = guideline?.success_criteria?.find((sc) => sc.ref_id === ref_id);
+
+	if (!criterion) {
+		throw new Error(`No WCAG item found with ref_id: ${ref_id}`);
+	}
+</script>
+
+{#if variant === 'link'}
+	<a href={criterion.url} class="link"
+		>WCAG
+		{criterion.ref_id}
+		{criterion.title}
+		({criterion.level})
+	</a>
+{:else if variant === 'card'}
+	<div class="card">
+		<a href={criterion.url}>{criterion.ref_id} {criterion.title} ({criterion.level})</a>
+		<p>{criterion.description}</p>
+		{#if criterion.special_cases}
+			<ul>
+				{#each criterion.special_cases as specialCase (specialCase.title)}
+					<li>{specialCase.description}</li>
+				{/each}
+			</ul>
+		{/if}
+	</div>
+{/if}
+
+<style>
+	.card {
+		border: 1px solid #ccc;
+		border-radius: 0.3rem;
+		padding: 0.5rem;
+	}
+</style>
